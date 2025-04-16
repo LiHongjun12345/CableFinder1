d0121c732cd0c5c6ae1249d94d6e33ecc916d5e1
LiHongjun12345 <79537426@qq.com>
2025-04-16 14:07:01 +0800

    模板修改数据库

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+const { sequelize } = require("./db");
 
 const logger = morgan("tiny");
 
@@ -21,25 +21,31 @@ app.get("/", async (req, res) => {
 app.post("/api/count", async (req, res) => {
   const { action } = req.body;
   if (action === "inc") {
-    await Counter.create();
+    await sequelize.create();
   } else if (action === "clear") {
-    await Counter.destroy({
+    await sequelize.destroy({
       truncate: true,
     });
   }
   res.send({
     code: 0,
-    data: await Counter.count(),
+    data: await sequelize.count(),
   });
 });
 
 // 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
+app.get("/api/CableList", async (req, res) => {
+  // const result = await sequelize.count();
+  try{
+    const pool = await sequelize;
+    const result = await pool.request().query('SELECT SN, CableUser0, CableUser1 FROM QR_info order by Timestamp desc');
+    return res.json(result.recordset);
+
+  }
+  catch(err){
+    console.error('Error fetching data:', err);
+    return res.status(500).send('Server error'); 
+  }
 });
 
 // 小程序调用，获取微信 Open ID
