0b3f6a28b5758e717d3420fe2b333903a914f02b
LiHongjun12345 <79537426@qq.com>
2025-04-17 15:56:40 +0800

    add sample list
    增加一个get  sample，用于将数据库中所有样件信息传给小程序

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get("/api/CableList", async (req, res) => {
 
 app.get("/api/sample", async (req, res) => {
   const result = await SampleManage.findAll({
-    attributes: ['ProjectName', 'SampleName', 'BOM'],
+    attributes: ['id', 'ProjectName', 'SampleName', 'StorageLocation', 'BOM', 'SN', 'Receiver', 'DeliveryDate', 'Comment'],
     raw: true
   });
   res.send({
